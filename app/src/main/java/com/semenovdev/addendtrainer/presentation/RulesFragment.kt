e0c961c4a7226aa5e0e0c3163511eff408075df5
SemenovDev <semenovdev@yahoo.com>
2025-04-21 17:46:56 +0600

    rules fragment

---
+++ app/src/main/java/com/semenovdev/addendtrainer/presentation/RulesFragment.kt
@@ -0,0 +1,17 @@
+package com.semenovdev.addendtrainer.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.semenovdev.addendtrainer.R
+
+class RulesFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_rules, container, false)
+    }
+}
